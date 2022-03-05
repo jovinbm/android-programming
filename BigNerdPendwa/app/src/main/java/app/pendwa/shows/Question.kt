411406bb6e5ce5c4beec26c0c2af84230982068b
Jovin Mwilanga <jovinbeda@gmail.com>
2022-03-05 13:55:51 -0500

    Saving project files

---
+++ BigNerdPendwa/app/src/main/java/app/pendwa/shows/Question.kt
@@ -0,0 +1,5 @@
+package app.pendwa.shows
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/criminalintent/CriminalListViewModel.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.criminalintent
+
+import androidx.lifecycle.ViewModel
+
+class CriminalListViewModel: ViewModel() {
+
+  private val crimeRepository = CrimeRepository.get()
+  val crimeListLiveData = crimeRepository.getCrimes()
+
+  fun addCrime(crime: Crime) {
+    crimeRepository.addCrime(crime)
+  }
+}
